ddb255d2150ae94837f2d6a2042619b8b8fc080d
Tin Ching Yuen <timching95@gmail.com>
2021-09-07 00:47:03 +0800

    install windi CSS

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<nav>
+	<a href=".">Home</a>
+	<a href="about">About</a>
+	<a href="settings" class="text-red-400 dark:text-green-400">Settings</a>
+</nav>
+<slot></slot>
+<style windi:preflights:global windi:safelist:global>
+</style>
